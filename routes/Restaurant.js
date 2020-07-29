296dbe9378b17452ced6806ee34cb10369c687c0
cabana <cabanaxsiempre@gmail.com>
2020-07-28 21:30:06 -0400

    creacion /routes/Restaurant.js (POST, GET, PUT, PATCH DELETE)

---
+++ routes/Restaurant.js
@@ -0,0 +1,107 @@
+var express = require("express");
+var router = express.Router();
+var REST = require("../database/restaurant");
+//GET mostrar
+router.get("/rest", (req, res) => {
+    var filter={};
+    var params= req.query;
+    var select="";
+    var order = {};
+    if(params.nombre_rest!=null){
+        var expresion =new RegExp(params.nombre_rest);
+        filter["nombre_rest"]=expresion;
+    }
+    if(params.filters!=null){
+        select=params.filters.replace(/,/g, " ");
+    }
+    if (params.order != null) {
+        var data = params.order.split(",");
+        var number = parseInt(data[1]);
+        order[data[0]] = number;
+    }
+    REST.find(filter).
+    select(select).
+    sort(order).
+    exec((err, docs)=>{
+        if(err){
+            res.status(500).json({msn: "Error en la coneccion del servidor"});
+            return;
+        }
+        res.status(200).json(docs);
+        return;
+    });
+});
+// POST registrar
+router.post("/rest", (req, res) => {
+    var userRest = req.body;
+    var userDB = new REST(userRest);
+    userDB.save((err, docs) => {
+        if (err) {
+            var errors = err.errors;
+            var keys = Object.keys(errors);
+            var msn = {};
+            for (var i = 0; i < keys.length; i++) {
+                msn[keys[i]] = errors[keys[i]].message;
+            }
+            res.status(500).json(msn);
+            return;
+        }
+        res.status(200).json(docs);
+        return;
+    })
+});
+//PUT actualizar
+router.put("/rest", async(req, res) => {
+    var params = req.query;
+    var bodydata = req.body;
+    if (params.id == null) {
+        res.status(300).json({msn: "El parámetro ID es necesario"});
+        return;
+    }
+    var allowkeylist = ["nombre_rest", "nit", "propietario","calle","telefono"];
+    var keys = Object.keys(bodydata);
+    var updateobjectdata = {};
+    for (var i = 0; i < keys.length; i++) {
+        if (allowkeylist.indexOf(keys[i]) > -1) {
+            updateobjectdata[keys[i]] = bodydata[keys[i]];
+        }
+    }
+    REST.update({_id:  params.id}, {$set: updateobjectdata}, (err, docs) => {
+       if (err) {
+           res.status(500).json({msn: "Existen problemas en la base de datos"});
+            return;
+        } 
+        res.status(200).json(docs);
+    });
+
+});
+//PATCH UPDATE
+router.patch("/rest", (req, res) => {
+    if (req.query.id == null) {
+        res.status(300).json({
+        msn: "no existe id"
+    });
+    return;
+    }
+    var id = req.query.id;
+    var params = req.body;
+    REST.findOneAndUpdate({_id: id}, params, (err, docs) => {
+        res.status(200).json(docs);
+    });
+});
+//DELETE 
+router.delete("/rest", (req, res) => {
+    var params = req.query;
+    if (params.id == null) {
+        res.status(300).json({msn: "El parámetro ID es necesario"});
+        return;
+    }
+    REST.remove({_id: params.id}, (err, docs) => {
+        if (err) {
+            res.status(500).json({msn: "Existen problemas en la base de datos"});
+             return;
+         } 
+         res.status(200).json(docs);
+    });
+});
+module.exports = router;
